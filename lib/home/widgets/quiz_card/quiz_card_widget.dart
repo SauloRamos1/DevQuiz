8d464249072beee2876d7fd73c8ca5a26dd1422d
SauloRamosGit <srdcp@hotmail.com>
2021-04-24 01:53:06 -0300

    Day 3

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,10 +1,22 @@
+import 'package:flutter/material.dart';
+
 import 'package:DevQuiz/core/app_colors.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
 import 'package:DevQuiz/core/core.dart';
 import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
-import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
+  final String title;
+  final String complete;
+  final double percent;
+
+  const QuizCardWidget({
+    Key? key,
+    required this.title,
+    required this.complete,
+    required this.percent,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -25,7 +37,7 @@ class QuizCardWidget extends StatelessWidget {
               height: 20,
             ),
             Text(
-              "Gerenciamento de Estado",
+              title,
               style: AppTextStyles.heading15,
             ),
             SizedBox(
@@ -36,14 +48,14 @@ class QuizCardWidget extends StatelessWidget {
                 Expanded(
                   flex: 1,
                   child: Text(
-                    "3 de 10",
+                    complete,
                     style: AppTextStyles.body11,
                   ),
                 ),
                 Expanded(
                     flex: 2,
                     child: ProgressIndicatorWidget(
-                      value: 0.3,
+                      value: percent,
                     ))
               ],
             )
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:DevQuiz/core/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:DevQuiz/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
